e522db1df27347305e2fd68ecd585c3d482d9600
Akash Das <akashdas.ny@gmail.com>
2020-01-07 23:12:07 -0500

    started question struct

--- player.c
+++ player.c
@@ -1,1 +1,10 @@
 #include "player.h"
+
+
+void increaseScore(struct Player * p, int increase){
+  p->score = p->score + increase;
+}
+
+void increaseRating(struct Player * p, int increase){
+  p->rating = p->rating + increase;
+}
--- player.h
+++ player.h
@@ -1,7 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <dirent.h>
+
+
 struct Player
 {
   char * username;
   int score;
   int rating;
   int answers[100]; //answers submitted
-}
+};
+
+void increaseScore(struct Player * p, int increase);
+void increaseRating(struct Player * p, int increase);
