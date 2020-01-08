51dffe92e3a48cb67732e151b95451b90c00aec3
SiLiang Lei <slei01@stuy.edu>
2020-01-08 00:27:04 -0500

    updated sorting alg

--- player.c
+++ player.c
@@ -20,7 +20,7 @@ int submit(struct Player * p, struct Question * q, int answer, int questionNum){
   return 0; // returns 0 if wrong
 }
 
-int sortPlayer(struct Player ** playerRankings){
+void sortPlayer(struct Player ** playerRankings){
   int i, swap;
   struct Player temp;
   while(1){
@@ -29,51 +29,11 @@ int sortPlayer(struct Player ** playerRankings){
       if(playerRankings[i]->score < playerRankings[i+1]->score){
         temp = playerRankings[i];
         playerRankings[i] = playerRankings[i+1];
-        playerRankings[i+1] = playerRankings[i];
+        playerRankings[i+1] = temp;
       }
     }
     if (swap == 0){
       break;
     }
   }
-}
-
-
-
-
-
-
-
-
-
-
-  if(playerRankings[0] -> score >= playerRankings[1] -> score >= playerRankings[0] -> score) {
-
-  }
-  if(playerRankings[0] -> score >= playerRankings[2] -> score >= playerRankings[1] -> score) {
-    p1 -> ranking = 1;
-    p2 -> ranking = 3;
-    p3 -> ranking = 2;
-  }
-  if(p2 -> score >= p1 -> score >= p3 -> score) {
-    p1 -> ranking = 2;
-    p2 -> ranking = 1;
-    p3 -> ranking = 3;
-  }
-  if(p2 -> score >= p3 -> score >= p1 -> score) {
-    p1 -> ranking = 3;
-    p2 -> ranking = 1;
-    p3 -> ranking = 2;
-  }
-  if(p3 -> score >= p1 -> score >= p2 -> score) {
-    p1 -> ranking = 2;
-    p2 -> ranking = 3;
-    p3 -> ranking = 1;
-  }
-  if(p3 -> score >= p2 -> score >= p1 -> score) {
-    p1 -> ranking = 3;
-    p2 -> ranking = 2;
-    p3 -> ranking = 1;
-  }
-}
 }
