69848143353b31bed2509833b75b43bdb4fd05f4
MarioTuMa <mtutuncumacias1@gmail.com>
2020-01-14 23:44:35 -0500

    Modified akash's standings code

--- server.c
+++ server.c
@@ -5,7 +5,6 @@
 #include <time.h>
 #include "player.h"
 
-void process(char *s);
 void getUser(struct Player *p);
 void readyMsg(int client_socket);
 void game(struct Player * players, int numPlayers, struct Question questions[], int numQuestions);
@@ -32,11 +31,11 @@ void sortPlayer(struct Player * playerRankings, int numPlayers){
 
 char * printPlayers(struct Player * playerRankings, int numPlayers) {
   sortPlayer(playerRankings, numPlayers);
-  char total[512];
+  char * total = malloc(50*(numPlayers+1));
   strcpy(total,"\n");
   int i = 0;
   while(i < numPlayers){
-    char info[50];
+    char * info = malloc(50);
     sprintf(info, "%d. %s: %d\n", i+1, playerRankings[i].username, playerRankings[i].score);
     strcat(total,info);
     i++;
@@ -155,10 +154,10 @@ void readyMsg(int client_socket) {
 }
 
 // void game(int * client_sockets, int numPlayers, struct Question questions[], int numQuestions) {
-  void game(struct Player * players, int numPlayers, struct Question questions[], int numQuestions) {
+void game(struct Player * players, int numPlayers, struct Question questions[], int numQuestions) {
   srand(time(0));
   int i;
-  int questionIndex = 0;
+  int questionIndex = rand() % numQuestions;;
   for(i=0;i<numPlayers;i++){
     char buffer[BUFFER_SIZE];
     // int client_socket=client_sockets[i];
@@ -166,53 +165,35 @@ void readyMsg(int client_socket) {
     strncpy(buffer, questions[questionIndex].problemText, sizeof(buffer));
     write(client_socket, buffer, sizeof(buffer));
   }
-  int * new = malloc(sizeof(int) * numPlayers);
+
   char rightMsgStart[200] = "Solved By ";
   char rightMsg[200];
   while(1){
 
     for(i=0;i<numPlayers;i++){
       char buffer[BUFFER_SIZE];
-      // int client_socket=client_sockets[i];
       int client_socket=players[i].socket;
-      if(new[i]){
-        //printf("We need to write to process %d\n",i);
-        strncpy(buffer, rightMsg, sizeof(buffer));
-        write(client_socket, buffer, sizeof(buffer));
-
-        char str[BUFFER_SIZE];
-        strcpy(str,printPlayers(players,numPlayers));
-        write(client_socket, str, sizeof(str));
-
-        //strncpy(buffer, "What is 2+2?", sizeof(buffer));
-
-        // questionIndex = rand() % numQuestions;
-
-        strncpy(buffer, questions[questionIndex].problemText, sizeof(buffer));
-        write(client_socket, buffer, sizeof(buffer));
-
-        int j;
-        new[i] = 0;
-      }
       int len = 0;
       ioctl(client_socket, FIONREAD, &len); //checks if stuff to read exists
       if(len) {
         read(client_socket, buffer, sizeof(buffer));
         if(!strcmp(buffer,questions[questionIndex].correctAnswer)){
           questionIndex = rand() % numQuestions;
-          // rightMsg[0] = players[i].username + '0';
           players[i].score = players[i].score + 1;
           if(strlen(rightMsg)){
               rightMsg[0]='\0';
           }
           strcat(rightMsg , rightMsgStart);
           strcat(rightMsg , players[i].username);
-
-
-          // rightMsg[0] = i + '0';
           int j;
           for(j=0;j<numPlayers;j++){
-            new[j]=1;
+            strncpy(buffer, rightMsg, sizeof(buffer));
+            write(players[j].socket, buffer, sizeof(buffer));
+            char str[BUFFER_SIZE];
+            strcpy(str,printPlayers(players,numPlayers));
+            write(players[j].socket, str, sizeof(str));
+            strncpy(buffer, questions[questionIndex].problemText, sizeof(buffer));
+            write(players[j].socket, buffer, sizeof(buffer));
           }
         }
       }
@@ -220,14 +201,3 @@ void readyMsg(int client_socket) {
   }
 }
 
-void process(char * s) {
-  char nextq[200] = "What is 2+2?";
-  char right[200] = " is right. ";
-  char wrong[200] = " is the wrong answer. The correct answer was 4.";
-  if(strcmp(s,"4")){
-    strcat(strcat(s,wrong),nextq);
-  }
-  else{
-    strcat(strcat(s,right),nextq);
-  }
-}
