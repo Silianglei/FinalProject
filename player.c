7ce53689ab410befc09e7291ab8fbca1c1d63806
Si Liang Lei <slei01@stuy.edu>
2020-01-16 11:40:01 -0500

    added library

--- player.c
+++ player.c
@@ -59,48 +59,6 @@ int submit(struct Player * p, struct Question * q, int answer, int questionNum){
   return 0; // returns 0 if wrong
 }
 
-// void sortPlayer(struct Player * playerRankings, int numPlayers){
-//   int i, swap;
-//   struct Player temp;
-//   while(1){
-//     swap = 0;
-//     for(i=0; i<numPlayers; i++){
-//       if(playerRankings[i].score < playerRankings[i+1].score){
-//         temp = playerRankings[i];
-//         playerRankings[i] = playerRankings[i+1];
-//         playerRankings[i+1] = temp;
-//         swap =1;
-//       }
-//     }
-//     if (swap == 0){
-//       break;
-//     }
-//   }
-// }
-
-// void printPlayers(struct Player * playerRankings, int numPlayers) {
-//   sortPlayer(playerRankings, numPlayers);
-//   int i = 0;
-//   while(i < numPlayers){
-//     printf("%d. %s: %d\n", i, playerRankings[i].username, playerRankings[i].score);
-//     i++;
-//   }
-// }
-
-// char * printPlayers(struct Player * playerRankings, int numPlayers) {
-//   sortPlayer(playerRankings, numPlayers);
-//   char total[512];
-//   int i = 0;
-//   while(i < numPlayers){
-//     char info[50];
-//     sprintf(info, "%d. %s: %d\n", i+1, playerRankings[i].username, playerRankings[i].score);
-//     strcat(total,info);
-//     i++;
-//   }
-//   // printf("%s", total);
-//   return total;
-// }
-
 int main(int argc, char **argv) {
 
   int server_socket;
